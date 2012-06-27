8dd5024e353d04bc4b4ddc8a0e080239cafc564d
Luca Menichetti <meniluca@gmail.com>
2012-06-27 12:50:03 +0200

    first commit

---
+++ villa_badoer.js
@@ -0,0 +1,52 @@
+/*
+	======================
+	
+	Villa Badoer
+	is a villa in Fratta Polesine in the Veneto region;
+	it was designed in 1556 by Andrea Palladio for the Venetian noble Francesco Badoer.
+	
+	======================
+
+	The villa is decomposed in 4 main parts: the main structure, the porches, the two barchessa and the garden.
+	See http://git.prdjs.com/418042/final-project/gitpages... for more information
+*/
+
+
+/*
+	Drawer object responsable to draw villa's parts.
+*/
+function Drawer(){
+	this.modelList = [];
+}
+
+Drawer.prototype.addModel = function(newModel){
+	this.modelList.push(newModel);
+}
+
+Drawer.prototype.draw = function(){
+	return DRAW(STRUCT(this.modelList));
+}
+
+
+/*
+	The main structure
+*/
+
+function mainStructure(){
+
+	var 
+
+	return 
+
+}
+
+/*
+	Function charged to create an object Drawer and draw the villa
+*/
+function drawVilla(){
+	var drawer = Drawer();
+	drawer.addModel(mainStructure());
+	drawer.draw();
+}
+
+drawVilla();
